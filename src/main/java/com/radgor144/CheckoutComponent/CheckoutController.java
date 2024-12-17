246e1258de463ee4e33381752e0f98334f1a8543
Radek <radek.goralik@interia.pl>
2024-12-17 20:07:14 +0100

    Added input validation to CheckoutController endpoints

--- src/main/java/com/radgor144/CheckoutComponent/CheckoutController.java
+++ src/main/java/com/radgor144/CheckoutComponent/CheckoutController.java
@@ -1,8 +1,14 @@
 package com.radgor144.CheckoutComponent;
 
 import com.radgor144.CheckoutComponent.CartComponents.CartResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Validated
 public class CheckoutController {
 
     private final CheckoutService checkoutService;
@@ -22,22 +29,26 @@ public class CheckoutController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> addToCart(@RequestParam int idCart, @RequestParam String item, @RequestParam int amount) {
+    public ResponseEntity<?> addToCart(@RequestParam @Valid @Min(1) int idCart,
+                                       @RequestParam @Valid @NotBlank @Size(min = 1, max = 40)  String item,
+                                       @RequestParam @Valid @Min(1) int amount) {
         return ResponseEntity.ok(checkoutService.addToCart(idCart, item, amount));
     }
 
     @GetMapping("/scan")
-    public ResponseEntity<CartResponse> scan(@RequestParam int idCart) {
+    public ResponseEntity<CartResponse> scan(@RequestParam @Valid @Min(1) int idCart) {
         return ResponseEntity.ok(checkoutService.getCartDetails(idCart));
     }
 
     @DeleteMapping("/remove")
-    public ResponseEntity removeFromCart(@RequestParam int idCart, @RequestParam String itemName, @RequestParam int amount) {
-        return checkoutService.removeFromCart(idCart, itemName, amount);
+    public ResponseEntity removeFromCart(@RequestParam @Valid @Min(1) int idCart,
+                                         @RequestParam @Valid @NotBlank @Size(min = 1, max = 40)  String item,
+                                         @RequestParam @Valid @Min(1) int amount) {
+        return checkoutService.removeFromCart(idCart, item, amount);
     }
 
     @DeleteMapping("/clear")
-    public ResponseEntity<?> clearCart(@RequestParam int idCart) {
+    public ResponseEntity<?> clearCart(@RequestParam @Valid @Min(1) int idCart) {
         checkoutService.clearCart(idCart);
         return ResponseEntity.ok("Cart cleared successfully.");
     }
